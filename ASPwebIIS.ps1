ae453a4264eef9b545628a310d3830afa6e29678
jeenam <jeenelsa@yahoo.com>
2016-03-11 20:20:06 -0500

    Powershell script file
    Power shell script to install IIS and replace Default website with Helloworld

---
+++ ASPwebIIS.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy Unrestricted
+Add-windowsfeature web-webserver -includeallsubfeature -logpath $env:temp\\webserver_addrole.log
+Add-windowsfeature web-mgmt-tools -includeallsubfeature -logpath $env:temp\\mgmttools_addrole.log
+Import-Module WebAdministration
+New-WebSite -Name Helloworld -Port 80 -HostHeader localhost -PhysicalPath "C:\inetpub\websitetodeploy"
+remove-website -name \"Default Web Site\"
+start-website -name Helloworld
